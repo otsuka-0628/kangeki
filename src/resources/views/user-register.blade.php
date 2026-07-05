24efc2294a6ee11d34ba850a7f7ff80c18988448
otsuka <nekoashi3105@yahoo.co.jp>
2026-07-05 10:16:41 +0900

    ホーム画面用ファイル追加・変更（DB未実装）

--- src/resources/views/user-register.blade.php
+++ src/resources/views/user-register.blade.php
@@ -4,11 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>新規登録</title>
+    @vite([
+        'resources/css/app.css',
+    ])
 </head>
 
 <body>
-
+    <div class="lr-bg" style="background-image: url('{{ asset('images/lr-background.jpg') }}')">
+        <section class=login-form>
+            <div class="form-root">
+                <img class="login-logo" src="images/logo2.png">
+                <form action="login-output.php" method="post">
+                    <input type="text" name="userID" placeholder="ユーザーID（メールアドレス）">
+                    <div class="password-group">
+                        <input type="password" name="password" placeholder="パスワード">
+                    </div>
+                    <div class="tp-group">
+                        <a href="{{ route('terms') }}">利用規約</a>
+                        <a href="{{ route('privacy') }}">プライバシーポリシー</a>
+                    </div>
+                    <input type="submit" value="新規登録">
+                </form>
+            </div>
+        </section>
+    </div>
 </body>
 
 </html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -14,6 +14,20 @@ Route::get('/user-login', function () {
     return view('user-login');
 })->name('user-login');
 
+Route::get('/forgot-password', function () {
+    return view('forgot-password');
+})->name('forgot-password');
+
+
 Route::get('/user-register', function () {
     return view('user-register');
 })->name('user-register');
+
+Route::get('/terms', function () {
+    return view('terms');
+})->name('terms');
+
+Route::get('/privacy', function () {
+    return view('privacy');
+})->name('privacy');
+
