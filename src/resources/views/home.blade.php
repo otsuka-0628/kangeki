6a83fc098062d1bd383ddb4cee7d8343304c221d
otsuka <nekoashi3105@yahoo.co.jp>
2026-07-20 12:39:02 +0900

    サイドバー実装

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KANGEKI</title>
     @vite([
-        'resources/css/register-top.css'
+        'resources/css/app.css'
     ])
 </head>
 
@@ -17,17 +17,8 @@
             {{ session('success') }}
         </div>
     @endif -->
-    <div class="container">
-        <nav class="sidebar">
-            <img class="sidebar-logo" src="images/logo-white.png">
-            <ul>
-                <li><a href='#'>劇団情報</a></li>
-                <li><a href='#'>予約フォーム作成</a></li>
-                <li><a href='#'>アカウント情報</a></li>
-                <li><a href='#'>お問い合わせ</a></li>
-                <li><a href='#'>退会</a></li>
-            </ul>
-        </nav>
+    <div class="dashboard-layout">
+        @include('sidebar')
 
         <main class="main-contents">
             <h2>予約受付中の公演</h2>
