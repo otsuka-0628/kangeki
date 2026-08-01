10c5b1449c2e8769d30d9eeaea2545ec0d9e0c6f
otsuka <nekoashi3105@yahoo.co.jp>
2026-08-01 18:03:08 +0900

    公演情報追加ページ仮作成

--- src/resources/views/performances/create.blade.php
+++ src/resources/views/performances/create.blade.php
@@ -68,22 +68,69 @@
                         <label class="form-label">日時別座席数</label>
                         <div id="seat-container">
                             <div class="seat-item dynamic-item" data-index="0">
-                                <label class="seat-label sub-label">【開演日時 1】の座席数：</label>
+                                <label class="seat-label sub-label">【開演日時 1】</label>
                                 <input type="number" name="schedules[0][capacity]" placeholder="例：50"
                                     class="form-control">
+
                             </div>
                         </div>
-
-                        <!-- チケット種類 -->
-                        <!-- チケット料金 -->
-                        <!-- 一人当たり予約上限枚数 -->
-                        <!-- 予約受付期限 -->
-                        <!-- 注意事項 -->
-
-
                     </div>
 
-                    <!-- 以下、同じような形でクラス化して作っていけばOK！ -->
+                    <!-- チケット種類 -->
+                    <div class="form-group">
+                        <div class="group-header">
+                            <label class="form-label">チケット種類</label>
+                            <button type="button" id="add-ticket-btn">＋チケット種類を追加</button>
+                        </div>
+                        <div id="ticket-type-container">
+                            <div class="ticket-type-item dynamic-item" data-index="0">
+                                <input type="text" class="ticket-type-input form-control" name="tickets[0][name]"
+                                    placeholder="例：一般、学生、前売り等">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- チケット料金 -->
+                    <div class="form-group">
+                        <label class="form-label">チケット料金</label>
+                        <div id="ticket-fee-container">
+                            <div class="ticket-fee-item" data-index="0">
+                                <label class="ticket-fee-label sub-label">【チケット種類 1】</label>
+                                <div class="input-unit-wrapper">
+                                    <input type="number" name="tickets[0][price]" placeholder="例：3000"
+                                        class="form-control"><span class="unit">円</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- 一人当たり予約上限枚数 -->
+                    <div class="form-group">
+                        <label class="form-label">一人当たり予約上限枚数</label>
+                        <div class="input-unit-wrapper">
+                            <input type="number" name="max_ticket_per_person"
+                                value="{{ old('max_ticket_per_person', 5) }}" min="1" class="form-control"><span
+                                class="unit">枚</span>
+                        </div>
+                    </div>
+
+                    <!-- 予約受付期限 -->
+                    <div class="form-group">
+                        <label class="form-label">予約受付期限</label>
+                        <input type="datetime-local" name="end-of-resarvation_at"
+                            value="{{ old('end_of_resarvation_at') }}" class="form-control">
+                    </div>
+
+                    <!-- 注意事項 -->
+                    <div class="form-group">
+                        <label class="form-label">注意事項</label>
+                        <textarea name="notes" rows="3" placeholder="開場時間や会場に関する注意事項など"
+                            class="form-control">{{ old('notes') }}</textarea>
+                    </div>
+
+                    <div class="form-submit">
+                        <input type="submit" value="登録" class="btn-submit">
+                    </div>
 
                 </div>
             </form>
