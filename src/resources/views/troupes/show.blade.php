d2efb57c50610a974c586c1bede0a7886d0ab511
otsuka <nekoashi3105@yahoo.co.jp>
2026-08-07 23:16:21 +0900

    劇団情報画面仮作成

--- src/resources/views/troupes/show.blade.php
+++ src/resources/views/troupes/show.blade.php
@@ -5,10 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>劇団情報</title>
+    @vite([
+        'resources/css/app.css'
+    ])
 </head>
 
 <body>
+    <div class="dashboard-layout">
+        @include('sidebar')
 
+        <div class="main-contents">
+            <h2>劇団情報</h2>
+            @if(!$troupe)
+
+                <!-- コンテナ名は仮 -->
+                <div class="troupe-container">
+                    <p>未登録です。</p>
+                    <a href="#" class="btn-troupe">
+                        劇団情報を登録
+                    </a>
+                </div>
+            @else
+
+            @endif
+
+        </div>
+    </div>
 </body>
 
 </html>
