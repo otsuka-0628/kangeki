ea568faeeb235942618b2d2f9bd91028f9d6882c
otsuka <nekoashi3105@yahoo.co.jp>
2026-08-11 22:04:05 +0900

    メニューに公演一覧追加

--- src/resources/views/sidebar.blade.php
+++ src/resources/views/sidebar.blade.php
@@ -2,6 +2,7 @@
     <img class="sidebar-logo" src="{{ asset('images/logo-white.png') }}">
     <ul>
         <li><a href="{{ route('troupe.show') }}">劇団情報</a></li>
+        <li><a href="{{ route('home') }}">公演一覧</a></li>
         <li><a href="{{ route('performances.create') }}">予約フォーム作成</a></li>
         <li><a href='#'>アカウント情報</a></li>
         <li><a href='#'>お問い合わせ</a></li>
