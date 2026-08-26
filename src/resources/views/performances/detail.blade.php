dc09327e36711b8f2d66a4940a0de3c4710a72c9
otsuka <nekoashi3105@yahoo.co.jp>
2026-08-26 22:14:16 +0900

    公演情報詳細　ボタン作成

--- src/resources/views/performances/detail.blade.php
+++ src/resources/views/performances/detail.blade.php
@@ -14,22 +14,38 @@
     <div class="dashboard-layout">
         @include('sidebar')
 
-        <div class="performance-container">
+        <div class="main-contents">
             <h2>公演情報詳細</h2>
 
-            <p class="performance-subtitle">{{ $performance->sub_title }}</p>
-            <p class="performance-title">{{ $performance->title }}</p>
-            @if($performance->schedules->isNotEmpty())
-                <p class="schedules-list">
-                <ul>
-                    @foreach($performance->schedules as $schedule)
-                        <li>{{ $schedule->start_at }}</li>
-                    @endforeach
-                </ul>
-                </p>
-            @else
-                <p>※開演日時の登録はありません。</p>
-            @endif
+            <div class="detail-container">
+
+                <div class="detail-buttons">
+                    <a href="#" class="btn btn-detail">予約フォームURL発行</a>
+                    <a href="#" class="btn btn-detail">予約者名簿抽出</a>
+                    <a href="#" class="btn btn-detail">編集</a>
+                    <form action="#" mathod="POST" onsubmit="return cunfirm('本当に削除しますか？')">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-delete">削除</button>
+                    </form>
+                </div>
+
+
+                <p class="performance-subtitle">{{ $performance->sub_title }}</p>
+                <p class="performance-title">{{ $performance->title }}</p>
+                @if($performance->schedules->isNotEmpty())
+                    <p class="schedules-list">
+                    <ul>
+                        @foreach($performance->schedules as $schedule)
+                            <li>{{ $schedule->start_at }}</li>
+                        @endforeach
+                    </ul>
+                    </p>
+                @else
+                    <p>※開演日時の登録はありません。</p>
+                @endif
+
+            </div>
         </div>
     </div>
 </body>
